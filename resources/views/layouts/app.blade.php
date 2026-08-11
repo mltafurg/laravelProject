2e68098b942d7df6616800c50798500a7b47719a
malaura_t <mltafurg@eafit.edu.co>
2026-08-11 14:23:10 -0500

    adding create product in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,7 @@
           <a class="nav-link active" href="{{ route('home.about') }}">About</a>
           <a class="nav-link active" href="{{ route('home.contact') }}">Contact</a>
           <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+          <a class="nav-link active" href="{{ route('product.create') }}">Create Product</a
         </div>
       </div>
     </div>
